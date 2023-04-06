10069b722c9b296f0f119c0950b6a7e0a48d55eb
ChanvoLegion <chanvo@qq.com>
2023-04-06 13:35:56 +0800

    update

---
+++ 1017.cpp
@@ -0,0 +1,28 @@
+//
+// Created by ChanvoBook on 2023/4/6.
+//
+#include "iostream"
+#include "queue"
+#include "stack"
+#include "vector"
+#include "algorithm"
+#include "cstring"
+#include "cmath"
+#include "set"
+#include "map"
+#include "numeric"
+#include "unordered_set"
+
+using namespace std;
+
+class Solution {
+public:
+    string baseNeg2(int n) {
+
+    }
+};
+
+int main() {
+    cout << Solution().baseNeg2(4);
+    return 0;
+}
---
+++ 1053.cpp
@@ -0,0 +1,32 @@
+//
+// Created by ChanvoBook on 2023/4/3.
+//
+#include "iostream"
+#include "queue"
+#include "stack"
+#include "vector"
+#include "algorithm"
+#include "cstring"
+#include "cmath"
+#include "set"
+#include "map"
+#include "numeric"
+#include "unordered_set"
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> prevPermOpt1(vector<int> &arr) {
+
+        for (int i = 0; i < arr.size() - 1; ++i) {
+
+        }
+    }
+};
+
+int main() {
+    vector<int> x = {1, 2, 3};
+    Solution().prevPermOpt1(x);
+    return 0;
+}
